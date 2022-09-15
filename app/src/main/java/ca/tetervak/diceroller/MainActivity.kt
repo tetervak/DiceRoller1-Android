1d9eebd1a1d60f7d2084e8a8cc03c8b5ad835234
Alexander Tetervak <tetervak@sheridancollege.ca>
2022-09-15 17:32:38 -0400

    switched the project to using ViewBinding

--- app/src/main/java/ca/tetervak/diceroller/MainActivity.kt
+++ app/src/main/java/ca/tetervak/diceroller/MainActivity.kt
@@ -2,10 +2,18 @@ package ca.tetervak.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ca.tetervak.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
     }
 }
