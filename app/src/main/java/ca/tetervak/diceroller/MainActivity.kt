778e13f2ce6a24caed765c4d4898049f836c1667
Alexander Tetervak <tetervak@sheridancollege.ca>
2022-11-15 20:27:12 -0500

    used state-flow

--- app/src/main/java/ca/tetervak/diceroller/MainActivity.kt
+++ app/src/main/java/ca/tetervak/diceroller/MainActivity.kt
@@ -6,7 +6,11 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import ca.tetervak.diceroller.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,18 +32,29 @@ class MainActivity : AppCompatActivity() {
             mainViewModel.reset()
         }
 
-        mainViewModel.liveRollData.observe(this){ rollData ->
-            if(rollData == null){
-                hideDiceRows()
-                hideResetButton()
-            } else {
-                showDiceRows()
-                val list = rollData.values
-                updateDiceImageRow(list)
-                updateDiceValueRow(list)
-                showResetButton()
+        // Start a coroutine in the lifecycle scope
+        lifecycleScope.launch {
+            // repeatOnLifecycle launches the block in a new coroutine every time the
+            // lifecycle is in the STARTED state (or above) and cancels it when it's STOPPED.
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                // Trigger the flow and start listening for values.
+                // Note that this happens when lifecycle is STARTED and stops
+                // collecting when the lifecycle is STOPPED
+                mainViewModel.flowRollData.collect { rollData ->
+                    // New value received
+                    if(rollData == null){
+                        hideDiceRows()
+                        hideResetButton()
+                    } else {
+                        showDiceRows()
+                        val list = rollData.values
+                        updateDiceImageRow(list)
+                        updateDiceValueRow(list)
+                        showResetButton()
+                    }
+                    updateTotal(rollData?.total ?: 0)
+                }
             }
-            updateTotal(rollData?.total ?: 0)
         }
     }
 
--- app/src/main/java/ca/tetervak/diceroller/MainViewModel.kt
+++ app/src/main/java/ca/tetervak/diceroller/MainViewModel.kt
@@ -1,26 +1,27 @@
 package ca.tetervak.diceroller
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ca.tetervak.diceroller.model.DiceGame
 import ca.tetervak.diceroller.model.RollData
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MainViewModel: ViewModel() {
 
     // in a perfect app, this game object should be in the data layer
     private val game = DiceGame(numberOfDice = 3)
 
-    private val _liveRollData = MutableLiveData<RollData?>(game.rollData)
-    val liveRollData: LiveData<RollData?> = _liveRollData
+    private val _flowRollData = MutableStateFlow<RollData?>(game.rollData)
+    val flowRollData: StateFlow<RollData?> = _flowRollData.asStateFlow()
 
     fun roll() {
         game.roll()
-        _liveRollData.value = game.rollData
+        _flowRollData.value = game.rollData
     }
 
     fun reset() {
         game.reset()
-        _liveRollData.value = game.rollData
+        _flowRollData.value = game.rollData
     }
 }
